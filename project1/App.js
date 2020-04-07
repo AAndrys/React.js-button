afd2f9f302f4bb715e50f212f8700ddb31f63540
AAndrys <63213899+AAndrys@users.noreply.github.com>
2020-04-07 19:42:43 +0200

    Add files via upload

--- project1/App.js
+++ project1/App.js
@@ -6,36 +6,82 @@ const buttontexton = "turn on!";
 const buttontextoff = "turn off!";
 const newcolorbutton = "rgb(165, 47, 47)";
 const colorbutton = "rgb(240, 128, 128)";
+const color = 'green';
+const color1 = 'red';
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = { 
       text: buttontexton,
-      color: colorbutton
+      color: colorbutton,
+      textp: "Waiting...",
+      colorp: color,
+      size: 40,
+      animation: "show 2s linear infinite"
     };
     this.changetext = this.changetext.bind(this);
     this.changecolor = this.changecolor.bind(this);
+    this.changesizedown = this.changesizedown.bind(this);
   }
   changetext() {
     const newtext = this.state.text === buttontexton ? buttontextoff : buttontexton;
-    this.setState({ text: newtext });
+    const newtextp = this.state.textp === "Waiting..." ? "Running!" : "Waiting...";
+    const newanim = this.state.animation === "show 2s linear infinite" ? " " : "show 2s linear infinite";
+    this.setState({ 
+      text: newtext,
+      textp: newtextp,
+      animation: newanim
+    });
   }
   changecolor() {
     const newcolor = this.state.color === colorbutton ? newcolorbutton : colorbutton;
-    this.setState({ color: newcolor });
+    const newcolorp = this.state.colorp === color ? color1 : color;
+    this.setState({ 
+      color: newcolor,
+      colorp: newcolorp
+    });
+  }
+  changesizedown() {
+  this.setState({ size: this.state.size - 5 })
+  //  alert("work");
+  }
+  changesizeup() {
+    this.setState({ size: this.state.size + 5 })
   }
   render() {
     return (
     <div className="App">
       <header className="App-header">
+        <button
+        className="fontsizedown"
+        onClick={() => {
+          this.changesizedown();
+        }}>
+        FONT SIZE DOWN
+        </button>
+        <button
+          className="fontsizeup"
+          onClick={() => {
+            this.changesizeup();
+          }}>
+          FONT SIZE UP
+        </button>
         <button 
+        className="turn"
         style={{background: this.state.color}}
         onClick={() => {
           this.changetext();
           this.changecolor();
         }}>
-         { this.state.text }
+        { this.state.text }
         </button>
+          <p style={{ 
+            color: this.state.colorp,
+            fontSize: `${this.state.size}px`,
+            animation: this.state.animation
+            }}>
+            { this.state.textp }
+          </p>
       </header>
     </div>
   )}
